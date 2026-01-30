7c679034fde2ee75ed7b96cc106e90cd839d4e59
Naren Dey <naren.dey2005@gmail.com>
2026-01-30 11:10:39 +0530

    Revert Map Style: Switch back to Standard OpenStreetMap (Light) from Dark Matter.

--- frontend/dashboard/src/components/Map.jsx
+++ frontend/dashboard/src/components/Map.jsx
@@ -44,11 +44,11 @@ const MapComponent = ({ tourists, geofences }) => {
                 </div>
             </div>
 
-            <MapContainer center={NETWORK_CENTER} zoom={15} scrollWheelZoom={true} className="h-full w-full bg-black">
-                {/* Dark Matter Skin */}
+            <MapContainer center={NETWORK_CENTER} zoom={15} scrollWheelZoom={true} className="h-full w-full">
+                {/* Standard OSM Skin */}
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
                 {/* Render GeoFences - Red Polygons */}
